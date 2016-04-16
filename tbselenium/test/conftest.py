1762d81b3e1baa9d7bdced06bb8577cf77774231
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-04-16 13:32:46 +0200

    Try caching TBB archives on Travis.
    Conflicts:
    	.travis.yml

--- tbselenium/test/conftest.py
+++ tbselenium/test/conftest.py
@@ -1,4 +1,13 @@
 from os import environ
+import tempfile
+from os.path import join, dirname
+from tbselenium import common as cm
+from tbselenium.test import TBB_PATH
+try:
+    from stem.process import launch_tor_with_config
+except ImportError:
+    pass
+
 try:
     from pyvirtualdisplay import Display
 except ImportError:  # we don't need/install it when running CI tests
@@ -8,7 +17,7 @@ except ImportError:  # we don't need/install it when running CI tests
 DEFAULT_XVFB_WIN_W = 1280
 DEFAULT_XVFB_WIN_H = 800
 
-test_conf = {"xvfb_display": None}
+test_conf = {}
 
 
 def start_xvfb(win_width=DEFAULT_XVFB_WIN_W,
@@ -23,12 +32,28 @@ def stop_xvfb(xvfb_display):
         xvfb_display.stop()
 
 
+def start_tor_with_default_ports():
+    custom_tor_binary = join(TBB_PATH, cm.DEFAULT_TOR_BINARY_PATH)
+    environ["LD_LIBRARY_PATH"] = dirname(custom_tor_binary)
+    temp_data_dir = tempfile.mkdtemp()
+    torrc = {'ControlPort': str(cm.DEFAULT_CONTROL_PORT),
+             'SOCKSPort': str(cm.DEFAULT_SOCKS_PORT),
+             'DataDirectory': temp_data_dir}
+    return launch_tor_with_config(config=torrc, tor_cmd=custom_tor_binary)
+
+
 def pytest_sessionstart(session):
     if "TRAVIS" not in environ and "NO_XVFB" not in environ:
         test_conf["xvfb_display"] = start_xvfb()
+    if "TRAVIS" in environ:
+        test_conf["tor_process"] = start_tor_with_default_ports()
 
 
 def pytest_sessionfinish(session, exitstatus):
-    xvfb_display = test_conf["xvfb_display"]
+    xvfb_display = test_conf.get("xvfb_display")
+    tor_process = test_conf.get("tor_process")
     if xvfb_display:
         stop_xvfb(xvfb_display)
+
+    if tor_process:
+        tor_process.kill()
