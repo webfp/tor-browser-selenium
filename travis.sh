6297ba8746c2f6614238923b23b7425b2e480baa
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-04-17 14:51:58 +0200

    Source shell script to set env varible.

--- travis.sh
+++ travis.sh
@@ -4,4 +4,4 @@ locale=`echo $tarball |cut -d'_' -f 2 | cut -d'.' -f 1`
 mkdir -p ${DOWNLOAD_DIR}
 [ ! -e ${DOWNLOAD_DIR}/$tarball ] && wget -P ${DOWNLOAD_DIR} ${TBB_ARCHIVE_URL}/${VERSION_ARCH}
 tar -xf ${DOWNLOAD_DIR}/$tarball -C $HOME
-export TBB_PATH=${HOME}/tor-browser_$locale
+TBB_PATH=${HOME}/tor-browser_$locale
