df590f57c84c6e16a33f5558744d14d10b89ef41
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-04-18 18:37:26 +0200

    Add test to visit check.tpo and interact with the language menu.

---
+++ tbselenium/examples/check_tpo.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser
+from tbselenium.tbdriver import TorBrowserDriver
+from selenium.webdriver.support.ui import Select
+from time import sleep
+
+
+def visit(tbb_dir):
+    url = "https://check.torproject.org"
+    with TorBrowserDriver(tbb_dir) as driver:
+        driver.load_url(url)
+        # Iterate over a bunch of locales from the drop-down menu
+        for lang_code in ["en_US", "fr", "zh_CN", "fa", "ru", "hi", "ja"]:
+            select = Select(driver.find_element_by_id("cl"))
+            select.select_by_value(lang_code)
+            sleep(1)
+            print("\n======== Locale: %s ========" % lang_code)
+            status = driver.find_element_by("h1.on")  # status text
+            print (status.text)  # should be "Congratulations..."
+            ip_address = driver.find_element_by(".content > p")  # IP address
+            print (ip_address.text)
+
+
+def main():
+    desc = "Visit check.torproject.org website"
+    parser = ArgumentParser(description=desc)
+    parser.add_argument('tbb_path')
+    args = parser.parse_args()
+    visit(args.tbb_path)
+
+if __name__ == '__main__':
+    main()
--- tbselenium/examples/screenshot.py
+++ tbselenium/examples/screenshot.py
@@ -1,24 +1,25 @@
 from argparse import ArgumentParser
-from os.path import dirname, join, realpath
+from os.path import dirname, join, realpath, getsize
 from tbselenium.tbdriver import TorBrowserDriver
 
 
-def take_screenshot(tbb_dir, url):
+def visit_and_capture(tbb_dir, url):
     """Take screenshot of the page."""
     out_img = join(dirname(realpath(__file__)), "screenshot.png")
     with TorBrowserDriver(tbb_dir) as driver:
         driver.load_url(url, wait_for_page_body=True)
         driver.get_screenshot_as_file(out_img)
-    print("Screenshot is saved as %s" % out_img)
+    print("Screenshot is saved as %s (%s bytes)" % (out_img, getsize(out_img)))
 
 
 def main():
-    parser = ArgumentParser(description="Take a screenshot using Tor Browser")
+    desc = "Take a screenshot using TorBrowserDriver"
+    default_url = "https://check.torproject.org"
+    parser = ArgumentParser(description=desc)
     parser.add_argument('tbb_path')
-    parser.add_argument('url', nargs='?',
-                        default="https://check.torproject.org")
+    parser.add_argument('url', nargs='?', default=default_url)
     args = parser.parse_args()
-    take_screenshot(args.tbb_path, args.url)
+    visit_and_capture(args.tbb_path, args.url)
 
 if __name__ == '__main__':
     main()
