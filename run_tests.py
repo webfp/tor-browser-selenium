13b2baa791596516a95206ea4d0cfd1af2897b04
gunesacar <gunesacar@users.noreply.github.com>
2016-04-21 13:32:49 +0200

    Use argparse in runtests.py. (#48)

--- run_tests.py
+++ run_tests.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python
-import sys
+from argparse import ArgumentParser
 from subprocess import call
 from os import environ
 from os.path import isdir, realpath, dirname, join
-# TODO: Do we still need this module or can we do all of that with py.test
-# Alternatively we can call py.test from this module
-# Validate input
-if len(sys.argv) != 2:
-    print "ERROR: the path to the Tor Browser Bundle directory is a required argument."
-    sys.exit(-1)
 
-if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-    print "This script runs all the tests of the tor-browser-selenium project.\n" \
-          "It requires as argument the path to a Tor Browser Bundle directory."
-    sys.exit(0)
 
-tbb_path = sys.argv[1]
-if not isdir(tbb_path):
-    print "ERROR: the argument does not point to a directory."
-    sys.exit(-1)
-environ['TBB_PATH'] = tbb_path
+desc = "Run all the TorBrowserDriver tests"
+parser = ArgumentParser(description=desc)
+parser.add_argument('tbb_path')
+args = parser.parse_args()
+
+if not isdir(args.tbb_path):
+    raise IOError("Please pass the path to Tor Browser Bundle")
+
+# TBB_PATH environment variable is used by the tests
+environ['TBB_PATH'] = args.tbb_path
 
 # Get test directory from path of this script
 file_path = dirname(realpath(__file__))
 test_dir = join(file_path, 'tbselenium', 'test')
 
-# Run all tests
-call(["python", "-m", "unittest", "discover", test_dir])
+# Run all the tests using py.test
+call(["py.test", "-s", "-v", "--cov=tbselenium", "--cov-report",
+      "term-missing", "--durations=10", test_dir])
