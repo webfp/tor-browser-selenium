f700608effd7e9344dff741e3d88308a02b75456
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-06-10 15:44:22 +0200

    Add double quotes, debug string concat issue.

--- travis.sh
+++ travis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mkdir -p ${DOWNLOAD_DIR}
+echo VERSION_ARCH
+echo ${TBB_ARCHIVE_URL}/${VERSION_ARCH}
 [ ! -e ${DOWNLOAD_DIR}/$TARBALL ] && wget -P ${DOWNLOAD_DIR} ${TBB_ARCHIVE_URL}/${VERSION_ARCH}
 tar -xf ${DOWNLOAD_DIR}/$TARBALL -C $HOME
