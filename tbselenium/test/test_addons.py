d0a01e6389e4298ade541d9a2ae64e2784d64997
gunesacar <gunesacar@users.noreply.github.com>
2016-06-10 15:17:21 +0200

    Add an argument to install browser extensions (#54)
    * Add an argument to install browser addons.
    
    * Add a sample extension to test the new `extensions` argument.
    
    Manually checked (new Hello World menu item) but we should
    rather automate that check and/or replace this extension
    with a simple-to-test one (e.g. text/link rewriting extensions).

--- tbselenium/test/test_addons.py
+++ tbselenium/test/test_addons.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 import pytest
 
@@ -65,6 +66,19 @@ class NoScriptTest(unittest.TestCase):
             webgl_support = driver.execute_script(self.WEBGL_CHECK_JS)
             self.assertIn("getSupportedExtensions", webgl_support)
 
+
+class CustomExtensionTest(unittest.TestCase):
+
+    def test_should_install_custom_extension(self):
+        # We test if we can install the extension.
+        # We should improve the test by checking the expected outcome.
+        test_dir = os.path.dirname(os.path.realpath(__file__))
+        xpi_name = "xulschoolhello1.xpi"  # sample extension taken from:
+        # https://developer.mozilla.org/en-US/Add-ons/Overlay_Extensions/XUL_School/Getting_Started_with_Firefox_Extensions#Extension_Contents  # noqa
+        xpi_path = os.path.join(test_dir, "test_data", xpi_name)
+        with TBDriverFixture(TBB_PATH,
+                             extensions=[xpi_path]) as driver:
+            driver.load_url_ensure(cm.CHECK_TPO_URL)
 
 if __name__ == "__main__":
     unittest.main()
