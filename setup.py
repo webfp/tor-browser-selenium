1be1f1039b07793f30427a325f1870d5b36405d5
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-09-09 16:31:31 +0200

    Update package metadata.

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 
 setup(
     name="tbselenium",
-    description="Tor Browser automation with Selenium.",
-    keywords=["tor", "selenium", "tor browser", "driver"],
+    description="Tor Browser automation with Selenium",
+    keywords=["tor", "selenium", "tor browser"],
     version=0.1,
+    url = 'https://github.com/webfp/tor-browser-selenium',
     packages=["tbselenium"],
     install_requires=[
         "selenium>=2.45.0,<3"
