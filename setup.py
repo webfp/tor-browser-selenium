a017d20b08727afa6e03331f5ab5d59c08e73692
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-09-09 16:22:06 +0200

    Add setup.cfg, set package version to 0.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="tbselenium",
     description="Tor Browser automation with Selenium.",
     keywords=["tor", "selenium", "tor browser", "driver"],
-    version=1.0,
+    version=0.1,
     packages=["tbselenium"],
     install_requires=[
         "selenium>=2.45.0,<3"
