886e60a8a0ca1314e158ca4b80e680114319b649
Gunes Acar <gunes.acar@esat.kuleuven.be>
2016-10-12 23:23:51 +0200

    Bump geckodriver to 0.11.1.

--- travis.sh
+++ travis.sh
@@ -6,8 +6,8 @@ echo ${TBB_ARCHIVE_URL}/${VERSION_ARCH}
 [ ! -e ${DOWNLOAD_DIR}/$TARBALL ] && wget -P ${DOWNLOAD_DIR} ${TBB_ARCHIVE_URL}/${VERSION_ARCH}
 tar -xf ${DOWNLOAD_DIR}/$TARBALL -C $HOME
 echo `which curl`
-latest_geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/v0.10.0/geckodriver-v0.10.0-linux64.tar.gz"
-echo `curl -s https://api.github.com/repos/mozilla/geckodriver/releases`
+# TODO: get the last version automatically
+latest_geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz"
 latest_geckodriver_tarball=`basename ${latest_geckodriver_url}`
 echo $latest_geckodriver_tarball
 wget -P ${DOWNLOAD_DIR} $latest_geckodriver_url
