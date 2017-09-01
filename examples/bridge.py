d9aef07f77a0d8bd7ddb84e0e614fbb6e578ed0c
Gunes Acar <gunes.acar@esat.kuleuven.be>
2017-09-01 19:53:31 -0400

    Add an argument to choose the bridge type.
    Add tests for different bridges.
    
    Fixes #39 and closes #85.

--- examples/bridge.py
+++ examples/bridge.py
@@ -1,18 +1,17 @@
 from argparse import ArgumentParser
 from tbselenium.tbdriver import TorBrowserDriver
 from time import sleep
+# Usage: python bridge.py /path/to/tor-browser-bundle bridge_type
+# bridge_type is one of: obfs3, obfs4, fte, meek-amazon, meek-azure 
 
-BRIDGE_TYPE_PREF = "extensions.torlauncher.default_bridge_type"
-
-
-def visit(tbb_dir):
+def visit_using_bridge(tbb_dir, bridge_type="meek-amazon"):
     url = "https://check.torproject.org"
     with TorBrowserDriver(
-        tbb_dir, pref_dict={BRIDGE_TYPE_PREF: "meek-amazon"}) as driver:
+        tbb_dir, default_bridge_type=bridge_type) as driver:
         driver.load_url(url)
         print driver.find_element_by("h1.on").text  # status text
-        # Leave some time to check the Tor circuit interface to
-        sleep(10)  # verify that the bridge is really used.
+        sleep(10)  # To verify that the bridge is indeed uses, go to
+        # Tor Network Settings dialog
          
 
 
@@ -20,8 +19,9 @@ def main():
     desc = "Visit check.torproject.org website using a Tor bridge"
     parser = ArgumentParser(description=desc)
     parser.add_argument('tbb_path')
+    parser.add_argument('bridge_type', nargs='?', default="meek-amazon")
     args = parser.parse_args()
-    visit(args.tbb_path)
+    visit_using_bridge(args.tbb_path, args.bridge_type)
 
 if __name__ == '__main__':
     main()
