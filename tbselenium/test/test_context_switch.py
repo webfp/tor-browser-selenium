94390ac18f3ed6394d09a46321be775dcc6655bd
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-01-21 23:07:32 -0500

    Add tests for basic context switching.

--- tbselenium/test/test_context_switch.py
+++ tbselenium/test/test_context_switch.py
@@ -2,19 +2,30 @@ import unittest
 from tbselenium.test import TBB_PATH
 from tbselenium.test.fixtures import TBDriverFixture
 from tbselenium.common import CHECK_TPO_URL
+from selenium.common.exceptions import WebDriverException
 
 # based on https://stackoverflow.com/a/6549765
 ENUMERATE_FONTS = """
-var font_enumerator = Components.classes["@mozilla.org/gfx/fontenumerator;1"]
-.getService(Components.interfaces.nsIFontEnumerator);
-return enumerator.EnumerateAllFonts({});"""
+  var font_enumerator = Components.classes["@mozilla.org/gfx/fontenumerator;1"]
+  .getService(Components.interfaces.nsIFontEnumerator);
+  return font_enumerator.EnumerateAllFonts({});"""
 
 COMPONENT_CLASSES_JS = "var c = Components.classes; return c;"
 
 
 class TestGeckoDriverChromeScript(unittest.TestCase):
 
-    def test_bundled_font_via_chrome_script(self):
+    def test_should_not_run_chrome_script_without_context_switch(self):
+        with self.assertRaises(WebDriverException):
+            with TBDriverFixture(TBB_PATH) as driver:
+                driver.execute_script(COMPONENT_CLASSES_JS)
+
+    def test_should_run_chrome_script(self):
+        with TBDriverFixture(TBB_PATH) as driver:
+            driver.set_context('chrome')
+            driver.execute_script(COMPONENT_CLASSES_JS)
+
+    def test_bundled_fonts_via_chrome_script(self):
         # do not disable this pref when crawling untrusted sites
         pref_dict = {"dom.ipc.cpows.forbid-unsafe-from-browser": False}
 
