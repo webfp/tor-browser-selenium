7b5b97b5b8b2c885f41f8d8d29d0bfa6dd10f9ad
kjmcgrinder <kjmcgrinder@gmail.com>
2019-05-01 20:14:07 -0400

    Update common.py

--- tbselenium/common.py
+++ tbselenium/common.py
@@ -5,9 +5,9 @@ from os import environ
 # Old TBB versions (V3.X or below) have different directory structures
 DEFAULT_TBB_BROWSER_DIR = 'Browser'
 DEFAULT_TBB_TORBROWSER_DIR = join('Browser', 'TorBrowser')
-DEFAULT_TBB_FX_BINARY_PATH = join('Browser', 'firefox')
+DEFAULT_TBB_FX_BINARY_PATH = join('Browser', 'firefox.exe')
 DEFAULT_TOR_BINARY_DIR = join(DEFAULT_TBB_TORBROWSER_DIR, 'Tor')
-DEFAULT_TOR_BINARY_PATH = join(DEFAULT_TOR_BINARY_DIR, 'tor')
+DEFAULT_TOR_BINARY_PATH = join(DEFAULT_TOR_BINARY_DIR, 'tor.exe')
 DEFAULT_TBB_DATA_DIR = join(DEFAULT_TBB_TORBROWSER_DIR, 'Data')
 DEFAULT_TBB_PROFILE_PATH = join(DEFAULT_TBB_DATA_DIR, 'Browser',
                                 'profile.default')
@@ -16,10 +16,12 @@ TB_CHANGE_LOG_PATH = join(DEFAULT_TBB_TORBROWSER_DIR,
                           'Docs', 'ChangeLog.txt')
 
 # Directories for bundled fonts - Linux only
+"""
 DEFAULT_FONTCONFIG_PATH = join(DEFAULT_TBB_DATA_DIR, 'fontconfig')
 FONTCONFIG_FILE = "fonts.conf"
 DEFAULT_FONTS_CONF_PATH = join(DEFAULT_FONTCONFIG_PATH, FONTCONFIG_FILE)
 DEFAULT_BUNDLED_FONTS_PATH = join('Browser', 'fonts')
+"""
 
 # SYSTEM TOR PORTS
 DEFAULT_SOCKS_PORT = 9050
