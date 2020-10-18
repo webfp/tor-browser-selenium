4bc721d366429d70c076871f4a1eb440d12f8809
Gunes Acar <gunes.acar@esat.kuleuven.be>
2020-10-18 11:51:11 +0200

    Add long description for the PyPI page

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
+    author='Gunes Acar',
     name="tbselenium",
     description="Tor Browser automation with Selenium",
     keywords=["tor", "selenium", "tor browser"],
-    version="0.5.1",
-    url = 'https://github.com/webfp/tor-browser-selenium',
+    version="0.5.2",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/webfp/tor-browser-selenium',
     packages=["tbselenium", "tbselenium.test"],
     install_requires=[
         "selenium>=3.14"
