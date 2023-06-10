eb37400f01d149f23a2eca8dcc5cbc6fb50d68f7
Gunes Acar <g.acar@cs.ru.nl>
2023-06-10 14:56:50 +0200

    Use geckodriver 0.31.0

--- tbselenium/test/test_env.py
+++ tbselenium/test/test_env.py
@@ -16,7 +16,10 @@ class EnvironmentTest(unittest.TestCase):
 
     def test_geckodriver_version(self):
         """Make sure that the right geckodriver version is installed."""
-        GECKODRIVER_VERSION_STR = "geckodriver 0.30.0"
+        # we use 0.31.0 to be able to interact with chrome content (e.g. about: pages)
+        # until the next ESR release (>102)
+        # See, https://github.com/mozilla/geckodriver/issues/2060#issuecomment-1298313743
+        GECKODRIVER_VERSION_STR = "geckodriver 0.31.0"
         gd_v_out = check_output(["geckodriver", "-V"]).decode("utf-8")
         self.assertIn(GECKODRIVER_VERSION_STR, gd_v_out.split("\n")[0])
 
