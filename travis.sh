72e79a19082bac3da5f4680739f9c110d8f9c1fa
Gunes Acar <g.acar@cs.ru.nl>
2024-11-24 22:10:14 +0100

    Update CI TB version to 14.0.2

--- travis.sh
+++ travis.sh
@@ -7,7 +7,8 @@ echo ${TBB_DIST_URL}/${VERSION_ARCH}
 tar -xf ${DOWNLOAD_DIR}/$TARBALL -C $HOME
 echo `which curl`
 
-geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz"
+geckodriver_version="v0.35.0"  # update test_env.py if you update this as well
+geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/${geckodriver_version}/geckodriver-${geckodriver_version}-linux64.tar.gz"
 geckodriver_tarball=`basename ${geckodriver_url}`
 echo $geckodriver_tarball
 wget -P ${DOWNLOAD_DIR} $geckodriver_url
